499ebd78bab6ca988bcb9c09d331ad3378c1cdb8
Karine Santos <89669182+jkss13@users.noreply.github.com>
2023-04-15 00:24:55 -0300

    3º commit

--- src/GCSM_Java/HelloWorld.java
+++ src/GCSM_Java/HelloWorld.java
@@ -4,5 +4,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Teste 2");
+        System.out.println("Teste 3");
     }
 }
